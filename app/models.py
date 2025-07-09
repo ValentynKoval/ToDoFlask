9885b5dc491f837a2f6349c203afae1a11c51e70
valentyn <J.K.play2015@gmail.com>
2025-07-09 20:43:39 +0300

    create models of user and tasks

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .extensions import db
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
