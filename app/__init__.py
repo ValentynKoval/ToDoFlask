e97272771bb8691ea5d2e89a789160781aa68acb
valentyn <J.K.play2015@gmail.com>
2025-07-09 20:32:41 +0300

    create app with created variables

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from app.config import Config
+from app.extensions import migrate, api, jwt, db
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app)
+    jwt.init_app(app)
+    api.init_app(app)
+
+    api.add_namespace(auth_ns, path='/auth')
+    api.add_namespace(todos_ns, path='/todos')
+
+    return app
